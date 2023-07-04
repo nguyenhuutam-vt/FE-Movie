11f623cdf4f26b13919c5323e9e442cc4c92b41e
ncn <namnguyencong23@gmail.com>
2023-07-04 16:55:49 +0700

    04/07

--- fe-moive/src/App.js
+++ fe-moive/src/App.js
@@ -2,13 +2,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Home } from './home/Home';
+import Login from './Auth/Login';
 
 
 function App() {
   return (
     <Routes >
       <Route path="/" element={<Home />} />
-
+      <Route path='/login' element={<Login/>} />
+      
     </Routes>
   );
 }
